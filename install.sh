49cb2f4296f180275557769dacfc2c607d0a6bef
Jakub Dorňák <jakub.dornak@qbsoftware.cz>
2024-03-14 00:25:12 +0100

    local setup

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ source install/check-minimum-requirements.sh
 # in order to determine whether or not the clickhouse version needs to be upgraded.
 source install/upgrade-clickhouse.sh
 source install/turn-things-off.sh
-source install/create-docker-volumes.sh
+#source install/create-docker-volumes.sh
 source install/ensure-files-from-examples.sh
 source install/check-memcached-backend.sh
 source install/ensure-relay-credentials.sh
